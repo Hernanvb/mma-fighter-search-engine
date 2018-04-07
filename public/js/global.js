0564b1d929a7cdb60a94b8350e3800acf941476b
Leo <hernanvb@gmail.com>
2018-04-07 05:51:45 +0000

    add public/js directory and show.scss file

---
+++ public/js/global.js
@@ -0,0 +1,37 @@
+// Navigation Variables
+var caret = $(".fa.fa-caret-down");
+var box = $(".account-box").hide();
+
+var nav = $(".fa.fa-bars");
+var menu = $(".menu").hide();
+var selector = '.ul-menu li';
+
+// Trigger Click Event For Navigation
+caret.click(function(e) {
+
+  e.preventDefault();
+
+  box.fadeToggle();
+
+});
+
+nav.click(function() {
+
+  menu.fadeToggle();
+
+});
+
+$(selector).on('click', function(){
+    $(selector).removeClass('active');
+    $(this).addClass('active');
+});
+
+// $( function() {
+//   $( "#tabs" ).tabs();
+// } );
+
+$("#tabs").tabs({
+    activate: function (event, ui) {
+        var active = $('#tabs').tabs('option', 'active');
+    }
+});
